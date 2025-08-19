8316464aa260e061fc8599cddbcfd05ad535bd4d
geovannifrnaca <geovannidefranca98@gmail.com>
2025-08-19 12:21:38 -0300

    key

--- bin/dart_assincronismo.dart
+++ bin/dart_assincronismo.dart
@@ -1,28 +1,60 @@
 import 'dart:convert';
 
+import 'package:dart_assincronismo/api_key.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
   requestDataAsync();
-  print("ola mundo");
-}
-
-void requestDate() async {
-  String url =
-      "https://gist.githubusercontent.com/geovannifranca/57933551e45ee9457e561c89daee0715/raw/6f75faa4b77d0568b8ef88454242dcb26f0bb9e0/accounts.json";
-  Future<http.Response> futureResponse = http.get(Uri.parse(url));
-  await futureResponse.then((value) {
-    List<dynamic> listAccounts = json.decode(value.body);
-    Map<String, dynamic> carla = listAccounts.firstWhere(
-      (element) => element["name"] == "Carla",
-    );
-    print(carla["balance"]);
+  sendDataAsync({
+    "id":"NEW001",
+    "name": "flutter",
+    "lastName":"dart",
+    "balance": 5000,
   });
+
 }
 
-void requestDataAsync() async {
+// void requestDate() async {
+//   String url =
+//       "https://gist.githubusercontent.com/geovannifranca/57933551e45ee9457e561c89daee0715/raw/6f75faa4b77d0568b8ef88454242dcb26f0bb9e0/accounts.json";
+//   Future<http.Response> futureResponse = http.get(Uri.parse(url));
+//   await futureResponse.then((value) {
+//     List<dynamic> listAccounts = json.decode(value.body);
+//     Map<String, dynamic> carla = listAccounts.firstWhere(
+//       (element) => element["name"] == "Carla",
+//     );
+//     print(carla["balance"]);
+//   });
+// }
+
+Future<List<dynamic>> requestDataAsync() async {
   String url =
       "https://gist.githubusercontent.com/geovannifranca/57933551e45ee9457e561c89daee0715/raw/6f75faa4b77d0568b8ef88454242dcb26f0bb9e0/accounts.json";
   http.Response response = await http.get(Uri.parse(url));
-  print(json.decode(response.body)[0]);
+  return json.decode(response.body);
+}
+
+Future<String> sendDataAsync(Map<String, dynamic> mapAccount) async{
+  List<dynamic> listAccounts = await requestDataAsync();
+  listAccounts.add(mapAccount);
+  String countent = json.encode(listAccounts);
+
+  String url =
+      "https://api.github.com/gists/57933551e45ee9457e561c89daee0715";
+  
+  http.Response response = await http.post(
+  Uri.parse(url),
+  headers:{"Authorization": "Bearer $githubuApiKey"},
+  body:json.encode({
+    "description": "account.json",
+    "public": true,
+    "files":{
+      "accounts.json": {
+        "content": countent,
+      },
+    }
+  }),
+  );
+  print(response.statusCode);
+  return countent;
 }
